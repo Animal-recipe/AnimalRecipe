74fd317d4ce41140ad09e905db1dada69cab39db
jiwoo <hjw6112@konkuk.ac.kr>
2020-10-26 02:47:42 +0900

    front and image error check

--- AnimalRecipe/urls.py
+++ AnimalRecipe/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/', include('contact.urls'), name='contact'),
-    path('recipe/', include('recipe.urls'), name='recipe'),
+    path('', include('recipe.urls'), name='recipe'),
 ]
