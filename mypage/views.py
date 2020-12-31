f7fa74b5117e2cfbc46829a7bb147226e6c7aeb0
yoonjeon <yoonjeon0413@google.com>
2021-01-01 05:53:17 +0900

    Feat myRecipe,myReview,myQuestion. Fix navBar, Question style

--- mypage/views.py
+++ mypage/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from question.models import Question, Answer
-from recipe.models import Recipe
-from review.models import Review
+from recipe.models import Recipe_Img, Recipe
+from review.models import Review, Review_Img
 from question.forms import QuestionForm
 from django.shortcuts import redirect
 from django.core.paginator import Paginator
-# Create your views here.
+
 def myQuestion(request):
-    questions = Question.objects.all()
-    myQuestions = questions.filter(author=request.user)
+    myQuestions = Question.objects.filter(author=request.user)
     page = request.GET.get('page', 1)
     paginator = Paginator(myQuestions, 10)
     pageObject = paginator.get_page(page)
@@ -22,3 +21,49 @@ def delete_myQuestion(request, question_id):
     else:
         question.delete()
     return redirect('mypage:myQuestion')
+
+def myRecipe(request):
+    myRecipes = Recipe.objects.filter(author=request.user)
+    img = Recipe_Img.objects.filter()
+    recipes_dict={}
+    for i in range(0, myRecipes.__len__()):
+        tmp = myRecipes[i]
+        for j in range(0, img.__len__()):
+            if img[j].recipe == tmp:
+                img_obj = img[j]
+        recipes_dict[myRecipes[i]] = img_obj.image.url
+    recipes = tuple(recipes_dict.items())
+    page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 12)
+    recipeList =paginator.get_page(page)
+    return render(request, "mypage/myRecipe.html",{"recipeList":recipeList})
+
+def delete_myRecipe(request, recipe_id):
+    recipe = Recipe.objects.get(pk=recipe_id)
+    if recipe.author_id != request.user.id:
+        return redirect('recipe:list_recipe')
+    recipe.delete()
+    return redirect('mypage:myRecipe')
+
+def myReview(request):
+    myReviews = Review.objects.filter(author=request.user)
+    img = Review_Img.objects.all()
+    review_dict={}
+    for i in range(0, myReviews.__len__()):
+        tmp = myReviews[i]
+        for j in range(0, img.__len__()):
+            if img[j].review == tmp:
+                img_obj = img[j]
+        review_dict[myReviews[i]] = img_obj.image.url
+    reviews = tuple(review_dict.items())
+    page = request.GET.get('page', 1)
+    paginator = Paginator(reviews, 12)
+    reviewList =paginator.get_page(page)
+    return render(request, "mypage/myReview.html",{"reviewList":reviewList})
+
+def delete_myReview(request, review_id):
+    review = Review.objects.get(pk=review_id)
+    if review.author_id != request.user.id:
+        return redirect('review:list_review')
+    review.delete()
+    return redirect('mypage:myReview')
